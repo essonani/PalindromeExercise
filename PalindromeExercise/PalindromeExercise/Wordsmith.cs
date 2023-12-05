a5c009650111d1c27952b1d10a01f591679bc2f3
essonani tchakpalla <essonani@hotmail.com>
2023-12-04 22:10:32 -0600

    wet

---
+++ PalindromeExercise/PalindromeExercise/Wordsmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class Wordsmith
+    {
+        public bool IsApalindrome(string word)
+        {
+            var reverded = "";
+            for (int i =  word.Length-1;i>=0; i--)
+            {
+                reverded += word[i];
+            }
+            if (reverded == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
